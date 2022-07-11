3fb78ed6ec340284b35900dd612cbe16cbac62e2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-11 17:31:48 -0400

    make functioning debug loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
+
+use std::fmt::Debug;
+
+use notan::prelude::*;
+
+use notan::draw::*;
+
 use rand::{thread_rng, Rng, prelude::ThreadRng};
 
-fn main() {
+#[derive(AppState)]
+struct State {
+    last_key: Option<KeyCode>,
+}
+
+
+fn setup() -> State {
+    
+    State {
+        last_key: None,
+    }     
+
+}
+
+
+fn update(app: &mut App, state: &mut State) {
+    
+    state.last_key = app.keyboard.last_key_released();
+
+    if app.keyboard.is_down(KeyCode::Escape) {
+        app.exit();
+        println!("quiting");
+        return;
+    }
+
 
     const ARRAY_LEN: usize = 1000;
 
@@ -19,11 +50,20 @@ fn main() {
     for _ in 0..ARRAY_LEN {
         let test_2 = my_array[randy.gen_range(0..ARRAY_LEN)];
 
-        println!("YOUR SPECIAL NUMBER IS! {}", test_2);
+        // println!("YOUR SPECIAL NUMBER IS! {}", test_2);
     }
 
-
-
     println!("Hello, world!");
+
+}
+
+#[notan_main]
+fn main() -> Result<(), String> {
+
+    // test_thing();
+
+    notan::init_with(setup)
+        .update(update)
+        .build()
 
 }
