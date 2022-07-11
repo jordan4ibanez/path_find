4e9283abc6b28f3d0afe46465dc8d5da6544eec4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-11 01:10:48 -0400

    little loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rand::{thread_rng, Rng, prelude::ThreadRng};
+
 fn main() {
+
+    let mut randy: ThreadRng = thread_rng();
+
+    
+
+    for i in 0..1000 {
+
+        let test: i8 = randy.gen_range(0..127);
+
+        println!("YOUR NUMBA! {}", i + i32::from(test));
+
+        // drop(test);
+    }
+
     println!("Hello, world!");
+
 }
