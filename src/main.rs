b473859cf4a9fd074126bd382252e783e38edd72
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-11 01:15:38 -0400

    Parse known size array

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,27 @@ use rand::{thread_rng, Rng, prelude::ThreadRng};
 
 fn main() {
 
+    const ARRAY_LEN: usize = 1000;
+
     let mut randy: ThreadRng = thread_rng();
 
+    let mut my_array: [i32; ARRAY_LEN] = [0; ARRAY_LEN];
     
 
-    for i in 0..1000 {
+    for i in 0..ARRAY_LEN {
 
-        let test: i8 = randy.gen_range(0..127);
+        let test: i32 = randy.gen_range(0..100);
 
-        println!("YOUR NUMBA! {}", i + i32::from(test));
-
-        // drop(test);
+        my_array[i] = test;
     }
+
+    for _ in 0..ARRAY_LEN {
+        let test_2 = my_array[randy.gen_range(0..ARRAY_LEN)];
+
+        println!("YOUR SPECIAL NUMBER IS! {}", test_2);
+    }
+
+
 
     println!("Hello, world!");
 
